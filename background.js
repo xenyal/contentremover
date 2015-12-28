2ed201d716aa257cfd1ca5575597f88857b21aaf
Vincent Li <vinceliii@hotmail.com>
2015-12-28 15:23:31 -0500

    Referenced chrome dev documentation

--- background.js
+++ background.js
@@ -16,5 +16,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 		var activeTab = tabs[0];
 		chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
 	});
-	
+
 });
+
+// for messaging between content script and background script, refer to 
+// https://developer.chrome.com/extensions/messaging
