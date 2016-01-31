8ccb0fdc9525204c5cbff0a51fc7c72aa32d9efd
Vincent Li <vinceliii@hotmail.com>
2016-01-31 00:26:03 -0500

    Changed nature of extension; it's made for Facebook Likes only now

--- content.js
+++ content.js
@@ -1,23 +1,24 @@
 //===-------- content.js - implements scan for deletion --------===//
 //
-//
-//
-//
-//
-//
-//
-//
+//	Searches the current Facebook page context for "Like" features
+//	and removes them. This happens on interval only on the client-
+//	side. Meanwhile, the user is still able to "Like" posts on the
+//	site - they just won't be able to see it. The recipient of the
+//	user's likes will receive the like regardless.
 //
 //===-----------------------------------------------------------===//
 
-function scan() {
-	var el = document.getElementsByTagName(target);
-	for(var i = 0; i < el.length; i++){
-	   if(el[i].className.indexOf(input) > -1)
-	   {
-		   	el[i].parentNode.removeChild(el[i]); 
-	   }
+function scanParallelIteration() {
+	var likeSentence = document.getElementsByClassName("UFILikeSentence"),
+		likeButton = document.getElementsByClassName("UFICommentLikeButton");
+
+	for(var i = 0; i < likeSentence.length; i++) {
+		likeSentence[i].remove();
+	}
+
+	for(var j = 0; j < likeButton.length; j++) {
+		likeButton[j].remove();
 	}
 }
 
-window.setInterval(scan, 1);
+window.setInterval(scanParallelIteration, 1);
